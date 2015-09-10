e9b02f8b4157a95155349ed3876788ad6ed86355
Micah Markman <micahhm@stanfordalumni.org>
2015-09-10 09:34:04 -0400

    finished web scraping homework

--- Assignments/07_web_scraping.py
+++ Assignments/07_web_scraping.py
@@ -1,112 +1,4 @@
 '''
-CLASS: Web Scraping with Beautiful Soup
-
-What is web scraping?
-- Extracting information from websites (simulates a human copying and pasting)
-- Based on finding patterns in website code (usually HTML)
-
-What are best practices for web scraping?
-- Scraping too many pages too fast can get your IP address blocked
-- Pay attention to the robots exclusion standard (robots.txt)
-- Let's look at http://www.imdb.com/robots.txt
-
-What is HTML?
-- Code interpreted by a web browser to produce ("render") a web page
-- Let's look at example.html
-- Tags are opened and closed
-- Tags have optional attributes
-
-How to view HTML code:
-- To view the entire page: "View Source" or "View Page Source" or "Show Page Source"
-- To view a specific part: "Inspect Element"
-- Safari users: Safari menu, Preferences, Advanced, Show Develop menu in menu bar
-- Let's inspect example.html
-'''
-
-# read the HTML code for a web page and save as a string
-with open('example.html', 'rU') as f:
-    html = f.read()
-
-# convert HTML into a structured Soup object
-from bs4 import BeautifulSoup
-b = BeautifulSoup(html)
-
-# print out the object
-print b
-print b.prettify()
-
-# 'find' method returns the first matching Tag (and everything inside of it)
-b.find(name='body')
-b.find(name='h1')
-
-# Tags allow you to access the 'inside text'
-b.find(name='h1').text
-
-# Tags also allow you to access their attributes
-b.find(name='h1')['id']
-
-# 'find_all' method is useful for finding all matching Tags
-b.find(name='p')        # returns a Tag
-b.find_all(name='p')    # returns a ResultSet (like a list of Tags)
-
-# ResultSets can be sliced like lists
-len(b.find_all(name='p'))
-b.find_all(name='p')[0]
-b.find_all(name='p')[0].text
-b.find_all(name='p')[0]['id']
-
-# iterate over a ResultSet
-results = b.find_all(name='p')
-for tag in results:
-    print tag.text
-
-# limit search by Tag attribute
-b.find(name='p', attrs={'id':'scraping'})
-b.find_all(name='p', attrs={'class':'topic'})
-
-# limit search to specific sections
-b.find_all(name='li')
-b.find(name='ul', attrs={'id':'scraping'}).find_all(name='li')
-
-'''
-EXERCISE ONE
-'''
-
-# find the 'h2' tag and then print its text
-
-# find the 'p' tag with an 'id' value of 'feedback' and then print its text
-
-# find the first 'p' tag and then print the value of the 'id' attribute
-
-# print the text of all four resources
-
-# print the text of only the API resources
-
-'''
-Scraping the IMDb website
-'''
-
-# get the HTML from the Shawshank Redemption page
-
-# convert HTML into Soup
-
-# run this code if you have encoding errors
-
-# get the title
-
-# get the star rating
-
-'''
-EXERCISE TWO
-'''
-
-# get the description
-
-# get the content rating
-
-# get the duration in minutes (as an integer)
-
-'''
 OPTIONAL WEB SCRAPING HOMEWORK
 
 First, define a function that accepts an IMDb ID and returns a dictionary of
@@ -129,76 +21,56 @@ a list in which each element is a dictionary of movie information.
 Finally, convert that list into a DataFrame.
 '''
 
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+def get_movie_info(imdb_id):
+    result = None #default return value, e.g. movie not found
+    url = 'http://www.imdb.com/title/{0}'.format(imdb_id)
+    r = requests.get(url)
+    if r.status_code == 200:
+        result = {'content_rating': None,
+                    'description': None,
+                    'duration': None,
+                    'star_rating': None,
+                    'title': None,
+                        'imdb_id': imdb_id}
+        soup = BeautifulSoup(r.text)
+
+        # get the content rating
+        content_rating_tag = soup.find(name='meta', attrs={'itemprop':'contentRating'})
+        if content_rating_tag is not None:            
+            result['content_rating'] = content_rating_tag['content']
+    
+        # get the description
+        description_tag = soup.find(name='p', attrs={'itemprop':'description'})
+        if description_tag is not None:
+            result['description'] = description_tag.text
+
+        # get the duration in minutes (as an integer)
+        duration_tag = soup.find(name='time', attrs={'itemprop':'duration'})
+        if duration_tag is not None:
+            duration_text = duration_tag['datetime']
+            if duration_text is not None:
+                result['duration'] = float(duration_text[2:-1])
+
+        # get the star rating
+        result['star_rating'] = float(soup.find(name='span', attrs={'itemprop':'ratingValue'}).text)
+
+        # get the title
+        result['title'] = soup.find(name='span', attrs={'itemprop':'name'}).text
 
 
-'''
-Another IMDb example: Getting the genres
-'''
+    return result
+    
+movie_list = []
+with open('/Users/Micah/Documents/GeneralAssembly/Data Science/DAT8/data/imdb_ids.txt', mode='rU') as f:
+    for row in f:
+        movie_info = get_movie_info(row)
+        if movie_info is not None:        
+            movie_list.append(movie_info)
 
-# read the Shawshank Redemption page again
-r = requests.get('http://www.imdb.com/title/tt0111161/')
-b = BeautifulSoup(r.text)
+movies = pd.DataFrame(movie_list)
 
-# only gets the first genre
-b.find(name='span', attrs={'class':'itemprop', 'itemprop':'genre'})
 
-# gets all of the genres
-b.find_all(name='span', attrs={'class':'itemprop', 'itemprop':'genre'})
-
-# stores the genres in a list
-[tag.text for tag in b.find_all(name='span', attrs={'class':'itemprop', 'itemprop':'genre'})]
-
-'''
-Another IMDb example: Getting the writers
-'''
-
-# attempt to get the list of writers (too many results)
-b.find_all(name='span', attrs={'itemprop':'name'})
-
-# limit search to a smaller section to only get the writers
-b.find(name='div', attrs={'itemprop':'creator'}).find_all(name='span', attrs={'itemprop':'name'})
-
-'''
-Another IMDb example: Getting the URLs of cast images
-'''
-
-# find the images by size
-results = b.find_all(name='img', attrs={'height':'44', 'width':'32'})
-
-# check that the number of results matches the number of cast images on the page
-len(results)
-
-# iterate over the results to get all URLs
-for tag in results:
-    print tag['loadlate']
-
-'''
-Useful to know: Alternative Beautiful Soup syntax
-'''
-
-# read the example web page again
-with open('example.html', 'rU') as f:
-    html = f.read()
-
-# convert to Soup
-b = BeautifulSoup(html)
-
-# these are equivalent
-b.find(name='p')    # normal way
-b.find('p')         # 'name' is the first argument
-b.p                 # can also be accessed as an attribute of the object
-
-# these are equivalent
-b.find(name='p', attrs={'id':'scraping'})   # normal way
-b.find('p', {'id':'scraping'})              # 'name' and 'attrs' are the first two arguments
-b.find('p', id='scraping')                  # can write the attributes as arguments
-
-# these are equivalent
-b.find(name='p', attrs={'class':'topic'})   # normal way
-b.find('p', class_='topic')                 # 'class' is special, so it needs a trailing underscore
-b.find('p', 'topic')                        # if you don't name it, it's assumed to be the class
-
-# these are equivalent
-b.find_all(name='p')    # normal way
-b.findAll(name='p')     # old function name from Beautiful Soup 3
-b('p')                  # if you don't name the method, it's assumed to be find_all
